b683264d2b926a838b1792b39fc767fd765cec94
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-07-19 15:42:36 -0400

    18-arreglando error content tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {
       fontFamily: {
-        satoshi: ["Satoshi", "sans-serif"],
-        inter: ["Inter", "sans-serif"],
+        satoshi: ['Satoshi', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
       }
     },
   },
   plugins: [],
 }
-

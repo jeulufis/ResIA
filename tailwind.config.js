3a45e024e390fabaea85ea1ce7f4962c13128a50
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-07-19 13:24:51 -0400

    10-fuente personalizada satoshi

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        satoshi: ["Satoshi", "sans-serif"],
+      }
+    },
   },
   plugins: [],
 }

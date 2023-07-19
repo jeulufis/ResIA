f3e812a552865238c9136f355f72bda8bc7e6163
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-07-19 16:59:08 -0400

    21-agregar estado al input url

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -3,6 +3,10 @@ import { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
 
 export default function Demo() {
+  const [article, setArticle] = useState({
+    url: "",
+  });
+
   return (
     <section className="mt-16 w-full max-w-xl">
       {/* Search */}
@@ -19,8 +23,10 @@ export default function Demo() {
           <input
             type="url"
             placeholder="Enter a URL"
-            value=""
-            onChange={() => {}}
+            value={article.url}
+            onChange={(e) => {
+              setArticle({ ...article, url: e.target.value });
+            }}
             required
             className="url_input peer"
           />

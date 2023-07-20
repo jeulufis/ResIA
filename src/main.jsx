1935e2432369b66fa88c49df17f5b73cfb247a79
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-07-19 21:17:26 -0400

    25-importar store

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,11 @@ import App from "./App.jsx";
 import "./index.css";
 
 import { Provider } from "react-redux";
+import { store } from "./store/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>

2f9852f9ba9e3965cb0697a09d27c63460c49800
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-07-20 20:15:10 -0400

    agregando direccion cuando aprete el boton del footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,10 @@ export default function Footer() {
           <button
             type="button"
             className="text-white mb-4 bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2"
-          >
+            onClick={() =>
+                window.open("https://github.com/itodev-source/minor", "_blank")
+              }
+         >
             <svg
               className="w-4 h-4 mr-2"
               aria-hidden="true"
